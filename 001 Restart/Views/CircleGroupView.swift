b80411c28ef207e92ef34398391cec7af3c3e171
Jay Bergonia <jaybergonia@Jays-Mac-mini.local>
2022-01-18 11:34:59 +0800

    CircleGroupView set

---
+++ 001 Restart/Views/CircleGroupView.swift
@@ -0,0 +1,39 @@
+//
+//  CircleGroupView.swift
+//  001 Restart
+//
+//  Created by Jay Bergonia on 1/18/22.
+//
+
+import SwiftUI
+
+struct CircleGroupView: View {
+    
+    // MARK: PROPERTIES
+    
+    @State var ShapeColor: Color
+    @State var ShapeOpacity: Double
+    
+    // MARK: BODY
+    var body: some View {
+        ZStack {
+            Circle()
+                .stroke(ShapeColor.opacity(ShapeOpacity), lineWidth: 40)
+                .frame(width: 260, height: 260, alignment: .center)
+            Circle()
+                .stroke(ShapeColor.opacity(ShapeOpacity), lineWidth: 80)
+                .frame(width: 260, height: 260, alignment: .center)
+        }// ZSTACK CIRCLE
+    }
+}
+
+struct CircleGroupView_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea(.all, edges: .all)
+            
+            CircleGroupView(ShapeColor: .white, ShapeOpacity: 0.2)
+        }
+    }
+}
